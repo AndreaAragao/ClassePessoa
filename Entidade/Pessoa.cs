6ca711dabe1a2ae9216dc8c1e2358000c8f72dda
Andréa <andreamaragao@gmail.com>
2023-03-13 22:29:40 -0300

    Adicionar arquivos de projeto.

---
+++ Entidade/Pessoa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Pessoas01.Util;
+
+namespace Pessoas01.Entidade
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public Genero Genero { get; set;}
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Pessoas01.Entidade;
+
+namespace Pessoas01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa andrea = new Pessoa();
+            Pessoa andreaClone = andrea;
+            Pessoa douglas = new Pessoa();
+            Pessoa pessoa = new Pessoa();
+
+            pessoa.Nome = "Bob";
+        }
+    }
+}
